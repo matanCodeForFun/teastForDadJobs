4397bc75c4cd3681d8ad3bbc5bfeaef7e9ad8776
Matan Dizengof <diz.matan@gmail.com>
2025-12-18 16:06:43 +0200

    i cange the name of the file

--- filesTest.py
+++ filesTest.py
@@ -5,7 +5,7 @@ CATEGORY = input("Enter the category to search: ")
 thing = input("What to find: ")
 
 
-df = pd.read_excel('example.xlsx', sheet_name=sheet_name)
+df = pd.read_excel('אקסל דיווח נתוני אשראי 2025.xlsx', sheet_name=sheet_name)
 
 results = df[df[CATEGORY].str.contains(thing, na=False)]
 
