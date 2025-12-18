5eca3ee3fb71fc0b7e3504939e31c62727b6addb
Matan Dizengof <diz.matan@gmail.com>
2025-12-18 16:19:04 +0200

    i cage the code for what file but this shit dont work

--- filesTest.py
+++ filesTest.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import os
 
 sheet_name = input("Enter the sheet name: ")
 CATEGORY = input("Enter the category to search: ")
 thing = input("What to find: ")
 
-
-df = pd.read_excel('אקסל דיווח נתוני אשראי 2025.xlsx', sheet_name=sheet_name)
+file = os.chdir('C:/Users/Matan/Documents/אקסל דיווח נתוני אשראי 2025')
+    
+df = pd.read_excel(file, sheet_name=sheet_name)
 
 results = df[df[CATEGORY].str.contains(thing, na=False)]
 
